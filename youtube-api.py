d9a703c32bb23edc96a15900018f21c1ea9532c7
Edgar Valdez <edgarvaldez99@gmail.com>
2024-01-21 17:45:34 -0300

    refactor: use YouTube Analytics instead Reporting

--- youtube-api.py
+++ youtube-api.py
@@ -13,10 +13,10 @@ CLIENT_SECRETS_FILE = os.path.join(BASE_DIR, "credentials", "creds.json")
 # user's account for both YouTube Data API resources and YouTube Analytics Data.
 SCOPES = [
     "https://www.googleapis.com/auth/yt-analytics.readonly",
-    "https://www.googleapis.com/auth/yt-analytics-monetary.readonly",
+    # "https://www.googleapis.com/auth/yt-analytics-monetary.readonly",
 ]
-API_SERVICE_NAME = "youtubereporting"
-API_VERSION = "v1"
+API_SERVICE_NAME = "youtubeAnalytics"
+API_VERSION = "v2"
 
 
 # Authorize the request and store authorization credentials.
@@ -26,61 +26,44 @@ def get_authenticated_service():
     return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
 
 
-# Remove keyword arguments that are not set.
-def remove_empty_kwargs(**kwargs):
-    good_kwargs = {}
-    if kwargs is not None:
-        for key, value in kwargs.items():
-            if value:
-                good_kwargs[key] = value
-    return good_kwargs
+def run_analytics_report(youtube_analytics, **kwargs):
+  # Call the Analytics API to retrieve a report. Pass args in as keyword
+  # arguments to set values for the following parameters:
+  #
+  #   * ids
+  #   * metrics
+  #   * dimensions
+  #   * filters
+  #   * start_date
+  #   * end_date
+  #   * sort
+  #   * max_results
+  #
+  # For a list of available reports, see:
+  # https://developers.google.com/youtube/analytics/v1/channel_reports
+  # https://developers.google.com/youtube/analytics/v1/content_owner_reports
+  analytics_query_response = youtube_analytics.reports().query(
+    **kwargs
+  ).execute()
 
+  for column_header in analytics_query_response.get('columnHeaders', []):
+    print(f"{column_header['name']}"),
+  print(" ")
 
-# Call the YouTube Reporting API's reportTypes.list method to retrieve report types.
-def list_report_types(youtube_reporting, **kwargs):
-    # Provide keyword arguments that have values as request parameters.
-    kwargs = remove_empty_kwargs(**kwargs)
-    results = youtube_reporting.reportTypes().list(**kwargs).execute()
-    reportTypes = results["reportTypes"]
-
-    if "reportTypes" in results and results["reportTypes"]:
-        reportTypes = results["reportTypes"]
-        for reportType in reportTypes:
-            print(
-                "Report type id: %s\n name: %s\n"
-                % (reportType["id"], reportType["name"])
-            )
-        return reportTypes[0]["id"]
-    else:
-        print("No report types found")
-        return False
-
-
-# Call the YouTube Reporting API's jobs.create method to create a job.
-def create_reporting_job(youtube_reporting, report_type_id, **kwargs):
-    # Provide keyword arguments that have values as request parameters.
-    kwargs = remove_empty_kwargs(**kwargs)
-
-    reporting_job = (
-        youtube_reporting.jobs()
-        .create(body=dict(reportTypeId=report_type_id, name="job1"), **kwargs)
-        .execute()
-    )
-
-    print(
-        'Reporting job "%s" created for reporting type "%s" at "%s"'
-        % (
-            reporting_job["name"],
-            reporting_job["reportTypeId"],
-            reporting_job["createTime"],
-        )
-    )
-    print(f"reporting_job={reporting_job}")
+  for row in analytics_query_response.get('rows', []):
+    for value in row:
+      print(f"{value}"),
+    print(" ")
 
 
 if __name__ == "__main__":
     youtube_reporting = get_authenticated_service()
-    report_type_id = list_report_types(youtube_reporting)
-    if report_type_id:
-        print(f"report_type_id={report_type_id}")
-        reporting_job = create_reporting_job(youtube_reporting, report_type_id)
+    run_analytics_report(
+       youtube_reporting,
+       ids="channel==MINE",
+       startDate="2024-01-01",
+       endDate="2024-01-25",
+       metrics="views,likes,estimatedMinutesWatched,averageViewDuration,subscribersGained",
+       dimensions="day",
+       sort="day",
+    )
